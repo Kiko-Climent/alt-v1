74d06c6c092e87e7cf101ba26b88522cf6c0e0e9
Kiko Climent <kikocliment@MacBook-Air-3.fritz.box>
2025-01-15 12:12:34 +0100

    Basic structure home

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+
+
+const Footer = () => {
+
+  return(
+    <div className="flex items-start px-4 py-2 text-white bg-[#000000] font-semibold">
+      <div className="flex mr-1">instagram |</div>
+      <div className="flex"> soundcloud</div>
+    </div>
+  )
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+
+
+const Header = () => {
+
+  return(
+    <div className="flex justify-between px-4 py-2 text-white bg-[#000000]">
+      <div className="flex font-semibold">
+        <h1>against low trends</h1>
+      </div>
+      <div className="flex font-semibold gap-1">
+        <div className="flex">about,</div>
+        <div className="flex">contact,</div>
+        <div className="flex">events</div>
+      </div>
+    </div>
+  )
+};
+
+export default Header;
